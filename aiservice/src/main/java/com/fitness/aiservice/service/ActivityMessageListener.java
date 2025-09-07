9c77e105b29386876a05fcf3185e32dfb73384d6
ayush2442 <ayushpandey2442@gmail.com>
2025-09-08 01:55:00 +0530

    aiservice - created service for process ai recommendations

--- aiservice/src/main/java/com/fitness/aiservice/service/ActivityMessageListener.java
+++ aiservice/src/main/java/com/fitness/aiservice/service/ActivityMessageListener.java
@@ -1,6 +1,7 @@
 package com.fitness.aiservice.service;
 
 import com.fitness.aiservice.model.Activity;
+import com.fitness.aiservice.model.Recommendation;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
@@ -16,6 +17,7 @@ public class ActivityMessageListener {
     @RabbitListener(queues = "activity.queue")
     public void processActvity(Activity activity) {
         log.info("Received activity for processing: {}", activity.getId());
-        log.info("Generated Recommendation: {}", aiService.generateRecommendation(activity));
+//        log.info("Generated Recommendation: {}", aiService.generateRecommendation(activity));
+        Recommendation recommendation = aiService.generateRecommendation(activity);
     }
 }

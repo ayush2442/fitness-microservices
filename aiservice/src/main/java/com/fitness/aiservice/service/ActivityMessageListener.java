27f0ff985394d4cbe679fbffafa67dc88af61caf
ayush2442 <ayushpandey2442@gmail.com>
2025-09-08 01:56:57 +0530

    aiservice - mapped repository for saving the ai recommendations to repository

--- aiservice/src/main/java/com/fitness/aiservice/service/ActivityMessageListener.java
+++ aiservice/src/main/java/com/fitness/aiservice/service/ActivityMessageListener.java
@@ -2,6 +2,7 @@ package com.fitness.aiservice.service;
 
 import com.fitness.aiservice.model.Activity;
 import com.fitness.aiservice.model.Recommendation;
+import com.fitness.aiservice.repository.RecommendationRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
@@ -13,11 +14,13 @@ import org.springframework.stereotype.Service;
 public class ActivityMessageListener {
 
     private final ActivityAIService aiService;
+    private final RecommendationRepository recommendationRepository;
 
     @RabbitListener(queues = "activity.queue")
     public void processActvity(Activity activity) {
         log.info("Received activity for processing: {}", activity.getId());
 //        log.info("Generated Recommendation: {}", aiService.generateRecommendation(activity));
         Recommendation recommendation = aiService.generateRecommendation(activity);
+        recommendationRepository.save(recommendation);
     }
 }
